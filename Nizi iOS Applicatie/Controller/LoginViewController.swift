eb8c06e347f4ce325ad0d5174160f1344282aa7e
Patrick Dammers <patrickdammers123@outlook.com>
2019-11-19 16:29:05 +0100

    Viewcontroller Loginpagina
    Viewcontroller Loginpagina aangemaakt

---
+++ Nizi iOS Applicatie/Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Nizi iOS Applicatie
+//
+//  Created by Informatica Haarlem on 19-11-19.
+//  Copyright © 2019 De Mobiele Jongens. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController : ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
