c11f54d46c8bb4c6c00bc8b7a5cc6a01b0a50a67
Patrick Dammers <patrickdammers123@outlook.com>
2019-11-22 12:58:29 +0100

    Koppelen Fields
    Localizable strings voor Nederlands en Base toegevoegd + Fields aan de code toegevoegd en gekoppeld

--- Nizi iOS Applicatie/Controller/LoginViewController.swift
+++ Nizi iOS Applicatie/Controller/LoginViewController.swift
@@ -11,9 +11,44 @@ import UIKit
 
 class LoginViewController : UIViewController {
     
+    @IBOutlet weak var applicationNameLabel: UILabel!
+    @IBOutlet weak var pageTitleLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var needHelpLabel: UILabel!
+    @IBOutlet weak var needHelpDescription: UILabel!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var nierstichtingWebImage: UIImageView!
+    @IBOutlet weak var nierstrichtingPhoneImage: UIImageView!
+    @IBOutlet weak var nierstrichtingMailImage: UIImageView!
+    
+    @IBOutlet weak var nierstichtingWebLabel: UILabel!
+    @IBOutlet weak var nierstichtingPhoneLabel: UILabel!
+    @IBOutlet weak var nierstichtingMailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setLanguageSpecificText()
+    }
+    
+    func setLanguageSpecificText() {
+        applicationNameLabel.text = NSLocalizedString("ApplicationName", comment: "Nizi")
+        pageTitleLabel.text = NSLocalizedString("LoginPageTitle", comment: "")
+        usernameLabel.text = NSLocalizedString("UsernameLabel", comment: "")
+        passwordLabel.text = NSLocalizedString("PasswordLabel", comment: "")
+        needHelpLabel.text = NSLocalizedString("NeedHelp", comment: "")
+        needHelpDescription.text = NSLocalizedString("NeedHelpSubtitle", comment: "")
+        LoginButton.setTitle("Login", for: .normal)
+        nierstichtingWebLabel.text = NSLocalizedString("NierstichtingWeb", comment: "")
+        nierstichtingPhoneLabel.text = NSLocalizedString("NierstichtingPhone", comment: "")
+        nierstichtingMailLabel.text = NSLocalizedString("NierstichtingMail", comment: "")
     }
     
     override func didReceiveMemoryWarning() {
