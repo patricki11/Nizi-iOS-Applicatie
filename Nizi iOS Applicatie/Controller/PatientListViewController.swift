cac518107fa16cd49e9c9f958ec6f5755d976811
Patrick Dammers <patrickdammers123@outlook.com>
2019-11-23 15:31:13 +0100

    Patientenlijst styling
    Start gemaakt aan de patientenlijst styling (Modellen moeten nog aangemaakt worden)

--- Nizi iOS Applicatie/Controller/PatientListViewController.swift
+++ Nizi iOS Applicatie/Controller/PatientListViewController.swift
@@ -11,4 +11,45 @@ import UIKit
 
 class PatientListViewController : UIViewController {
     
+    @IBOutlet weak var applicationNameLabel: UILabel!
+    @IBOutlet weak var pageTitleLabel: UILabel!
+    
+    @IBOutlet weak var searchPatientField: UITextField!
+    @IBOutlet weak var addPatientButton: UIImageView!
+    
+    @IBOutlet weak var patientListTableView: UITableView!
+    
+    var patientList : [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        patientListTableView.delegate = self
+        patientListTableView.dataSource = self
+    }
+}
+
+
+extension PatientListViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return patientList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: Tableviewcell vullen en terugsturen
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt  indexPath: IndexPath) {
+        // TODO: Volgende patienten ophalen
+    }
+}
+
+extension PatientListViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TO DO: Go to Patient
+    }
 }
