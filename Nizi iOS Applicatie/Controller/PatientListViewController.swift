342aea598df85750024154644348eaaaed606ac8
Patrick Dammers <patrickdammers123@outlook.com>
2019-11-23 14:39:17 +0100

    Headergrootte loginpagina
    Grootte van header verkleind zoals in het design

---
+++ Nizi iOS Applicatie/Controller/PatientListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PatientListViewController.swift
+//  Nizi iOS Applicatie
+//
+//  Created by Informatica Haarlem on 23-11-19.
+//  Copyright © 2019 De Mobiele Jongens. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PatientListViewController : UIViewController {
+    
+}
