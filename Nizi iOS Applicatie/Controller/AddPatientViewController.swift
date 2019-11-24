789fa2588604f6a9f59652808c14dfd435c79154
Patrick Dammers <patrickdammers123@outlook.com>
2019-11-24 10:46:23 +0100

    Start met Toevoegen Patient
    Scherm aangemaakt voor het aanmaken van een nieuwe patient, het grootste gedeelte van de interface staat, alleen Richtlijnen moeten nog gedaan worden. (nog niets in de code)

---
+++ Nizi iOS Applicatie/Controller/AddPatientViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddPatientViewController.swift
+//  Nizi iOS Applicatie
+//
+//  Created by Informatica Haarlem on 24-11-19.
+//  Copyright © 2019 De Mobiele Jongens. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddPatientViewController : UIViewController {
+    
+}
